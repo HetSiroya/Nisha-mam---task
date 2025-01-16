611fba213554ad40232e9c7d819c07c40b134206
Siroya Het <hetsiroya@gmail.com>
2025-01-16 12:51:21 +0530

    first commit

---
+++ boostrap.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+  <div class="container-fluid"> <!-- Added container-fluid -->
+    <div class="row g-0"> <!-- Added g-0 for gap removal -->
+      <div class="col-xl-1 col-lg-2 col-md-3 col-sm-6 bg-danger text-light">1</div>
+      <div class="col-xl-1 col-lg-2 col-md-3 col-sm-6 bg-light">2</div>
+      <div class="col-xl-1 col-lg-2 col-md-3 col-sm-6 bg-danger text-light">3</div>
+      <div class="col-xl-1 col-lg-2 col-md-3 col-sm-6">4</div>
+      <div class="col-xl-1 col-lg-2 col-md-3 col-sm-6 bg-danger text-light">5</div>
+      <div class="col-xl-1 col-lg-2 col-md-3 col-sm-6">6</div>
+      <div class="col-xl-1 col-lg-2 col-md-3 col-sm-6 bg-danger text-light">7</div>
+      <div class="col-xl-1 col-lg-2 col-md-3 col-sm-6">8</div>
+      <div class="col-xl-1 col-lg-2 col-md-3 col-sm-6 bg-danger text-light">9</div>
+      <div class="col-xl-1 col-lg-2 col-md-3 col-sm-6">10</div>
+      <div class="col-xl-1 col-lg-2 col-md-3 col-sm-6 bg-danger text-light">11</div>
+      <div class="col-xl-1 col-lg-2 col-md-3 col-sm-6">12</div>
+    </div>
+  </div>
+</body>
+</html>
